e759a3f357a6b1fa28fd32f3b2674f39eede405f
manfred.rosskamp <manfred.rosskamp@531b2a56-492d-0410-a97d-d1b5483d4747>
2007-04-10 11:46:20 +0000

    Subversion test

--- SVNDemo/src/SVNDemo.java
+++ SVNDemo/src/SVNDemo.java
@@ -3,11 +3,11 @@ public class SVNDemo {
 	
 	public SVNDemo() {
 		System.out.println("SVNDemo");
-		addiere();
+		addiere(3, 5);
 	}
 	
-	public void addiere() {
-		System.out.println(5 + 8);
+	public void addiere(int zahl1, int zahl2) {
+		System.out.println(zahl1 + zahl2);
 	}
 	
 	public static void main(String[] args) {
