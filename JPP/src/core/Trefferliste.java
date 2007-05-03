907ecb07d6df4850af2222ecc7abcbd7b7c8a996
manfred.rosskamp <manfred.rosskamp@531b2a56-492d-0410-a97d-d1b5483d4747>
2007-05-03 19:33:23 +0000

    Erweiterung und Kommentierung der Dummy-Core-Klassen.

--- JPP/src/core/Trefferliste.java
+++ JPP/src/core/Trefferliste.java
@@ -9,27 +9,65 @@
 
 package core;
 
+import java.io.IOException;
+
+import org.apache.lucene.search.Hits;
+
 /**
- *
- * @author Nils
+ * Ein Objekt dieser Klasse stellt die Trefferliste sortiert nach der 
+ * Treffergenauigkeit einer Suche dar.
+ * @author Manfred Rosskamp
+ * @author Nils Verheyen
  */
 public class Trefferliste {
   
+  /** Enthaelt die Trefferliste, der Lucenesuche. */
   private Hits treffer;
   
-  /** Creates a new instance of Trefferliste */
+  /**
+   * Erzeugt eine neue Trefferliste
+   */
   public Trefferliste() {
   }
   
+  /**
+   * Gibt die Anzahl der Treffer zurueck.
+   * @return Anzahl der Treffer
+   */
   public int getAnzahlTreffer() {
-    return 0;
+    return treffer.length();
   }
   
+  /**
+   * Gibt den Treffer mit der Nummer <code>treffernummer</code> zurueck.
+   * @param treffernummer  Nummer des Treffers
+   * @return BildDokument dieser Trefferlist an der Stelle treffernummer
+   */
   public BildDokument getBildDokument(int treffernummer) {
-    
+    try {
+      return BildDokument.erzeugeAusLucene(treffer.doc(treffernummer));
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return null;
   }
   
+  /**
+   * Gibt die Treffergenauigkeit mit der Nummer <code>treffernummer</code> 
+   * zurueck. Die Treffergenauigkeit liegt zwischen 1 und 0, wobei 1 die beste
+   * Treffergenauigkeit angibt. -1 wird bei einem Fehler zurueckgegeben.
+   * @param treffernummer  Nummer des Treffers
+   * @return  Treffergenauigkeit des Treffers mit der Nummer 
+   *      <code>treffernummer</code>
+   */
   public float getTrefferGenauigkeit(int treffernummer) {
-    
+    try {
+      return treffer.score(treffernummer);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return -1;
   }
 }
