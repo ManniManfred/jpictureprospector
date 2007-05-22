dd07b349a6128ad658191d7eba08f2be9968a510
nverheyen <nverheyen@531b2a56-492d-0410-a97d-d1b5483d4747>
2007-05-22 21:20:10 +0000

    update UI

--- trunk/JPP/src/ui/Thumbnail.java
+++ trunk/JPP/src/ui/Thumbnail.java
@@ -1,6 +1,7 @@
 package ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -30,16 +31,8 @@ public class Thumbnail extends JPanel {
     init(thumbnail, new Dimension(75, 75));
   }
   
-  /**
-   * Erzeugt ein neues Objekt der Klasse.
-   * 
-   * @param thumbnail  enthaelt das anzuzeigende Thumbnail
-   * @param groesze  enthaelt die Groesze in der das Thumbnail angezeigt
-   *        werden soll
-   */
-  public Thumbnail(Image thumbnail, Dimension groesze) {
-    
-    init(thumbnail, groesze);
+  public void setzeGroesze(Dimension groesze) {
+    this.groesze = groesze;
   }
   
   public void init(Image thumbnail, Dimension groesze) {
@@ -53,14 +46,23 @@ public class Thumbnail extends JPanel {
   
   protected void paintComponent(Graphics g) {
     
-    if (thumbnail != null) {
+    double hoeheBild = this.thumbnail.getHeight(this);
+    double breiteBild = this.thumbnail.getWidth(this);
+    double dieseBreite = getWidth();
+    double dieseHoehe = getHeight();
+    g.setColor(new Color(238, 238, 238));
+    g.fillRect(0, 0, getWidth(), getHeight());
+    
+    // Anpassung der Größe an dieses Objekt
+    if (Math.abs(dieseBreite / breiteBild) < Math.abs(dieseHoehe / hoeheBild)) {
       
-      g.drawImage(this.thumbnail, 
-          (int) (this.getWidth() - this.thumbnail.getWidth(this)) / 2, 
-          (int) (this.getHeight() - this.thumbnail.getHeight(this)) / 2,
-          (int) groesze.getWidth(),
-          (int) groesze.getHeight(),
-          this);
+      g.drawImage(this.thumbnail, 0, 0, (int) dieseBreite,
+          (int) (hoeheBild * (dieseBreite / breiteBild)), this);
+    } else {
+      
+      g.drawImage(this.thumbnail, 0, 0,
+          (int) (breiteBild * (dieseHoehe / hoeheBild)),
+          (int) dieseHoehe, this);
     }
   }
 }
