4c4c7b6546862aa1053825c03003a457f92cfd08
marion.mecking <marion.mecking@531b2a56-492d-0410-a97d-d1b5483d4747>
2007-05-27 12:45:10 +0000

    Thumbnail neu

--- trunk/JPP/src/merkmale/ThumbnailMerkmal.java
+++ trunk/JPP/src/merkmale/ThumbnailMerkmal.java
@@ -1,49 +1,51 @@
 package merkmale;
 
 import core.GeoeffnetesBild;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
-import javax.swing.ImageIcon;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 
 
 /**
- * Ein Objekt dieser Klasse stellt das Thumbnail eines 
+ * Ein Objekt dieser Klasse stellt das Thumbnail eines
  * Bilddokumentes dar.
  * @author Marion Mecking
  */
 public class ThumbnailMerkmal extends Merkmal {
-   
+  
   /** Name des Lucene-Feldes fuer dieses Merkmal. Sollte die Konstante
-   * geaendert werden, muss auch in der Klasse 
+   * geaendert werden, muss auch in der Klasse
    * <code>ThumbnailAnzeigePanel</code> die Konstante
    * <code>THUMBNAILMERKMALSNAME</code> entsprechend angepasst werden. */
   public static final String FELDNAME = "Thumbnail";
   
   /** Konstante zur Umrechnung von Byte in kB. */
-  private Image thumbnail;
+  private BufferedImage thumbnail;
   
   /** Maximale Hoehe des Thumbnails in Pixeln. */
-  private static final int MAXHOEHE = 100;
-
-  /** Maximale Breite des Thumbnails in Pixeln. */
-  private static final int MAXBREITE = 100;
+  private static final int MAXHOEHE = 256;
   
-  /** 
+  /** Maximale Breite des Thumbnails in Pixeln. */
+  private static final int MAXBREITE = 256;
+  
+  /**
    * Erzeugt ein neues ThumbnailMerkmal.
    */
   public ThumbnailMerkmal() {
     super(FELDNAME);
-  }   
+  }
   
   /**
    * Liest das Thumbnail aus dem uebergebenen geoeffneten
    * Bild und speichert diesen in diesem Merkmal-Objekt.
    * @param bild  Bild, aus dem der Merkmalswert gelesen wird
    */
-  public void leseMerkmalAus(GeoeffnetesBild bild) { 
- 
+  public void leseMerkmalAus(GeoeffnetesBild bild) {
+    
     BufferedImage foto = bild.getBild();
     
     if (foto.getWidth() > foto.getHeight()) {
@@ -51,39 +53,58 @@ public class ThumbnailMerkmal extends Merkmal {
       /* Bild ist im Querformat, Thumbnail bekommt maximale Breite, H�he
        * wird proportional zur Breite berechnet
        */
-      thumbnail = foto.getScaledInstance(MAXBREITE, -1, foto.SCALE_DEFAULT);
+      thumbnail = (BufferedImage) 
+        foto.getScaledInstance(MAXBREITE, -1, foto.SCALE_DEFAULT);
     } else {
       
       /* Bild ist im Hochformat, Thumbnail bekommt maximale H�he, Breite
        * wird proportional zur Breite berechnet
        */
-      thumbnail = foto.getScaledInstance(-1, MAXHOEHE, foto.SCALE_DEFAULT);
+      thumbnail = (BufferedImage) 
+        foto.getScaledInstance(-1, MAXHOEHE, foto.SCALE_DEFAULT);
     }
     
-    /* Thumbnail in ein Byte-Array schreiben */   
-    //ByteArrayOutputStream bas = new ByteArrayOutputStream();
-    //ImageIO.write(thumbnail, "pnm", bas);
-    //byte[] data = bas.toByteArray();
-    //this.wert =  ??
-  }
+    /* Thumbnail in ein Byte-Array schreiben */
+    
 
+    try{
+    // O P E N
+    ByteArrayOutputStream baos = new ByteArrayOutputStream( 1000 );   
+    // W R I T E
+    ImageIO.write(this.getThumbnail(), "jpeg", baos ); 
+    /* "png" "jpeg" format desired, no "gif" yet. */ 
+    // C L O S E
+    baos.flush();
+    this.wert = baos.toByteArray();
+    
+    baos.close();
+    } catch(IOException e) {
+      // Exception werfen
+    }
+  }
+  
   /**
-   * Liest den Merkmalswert aus einem Lucene-Document und speichert diesen in 
+   * Liest den Merkmalswert aus einem Lucene-Document und speichert diesen in
    * diesem Merkmal-Objekt.
-   * @param doc  Lucene-Document, von dem ueber ein zu diesem Merkmal 
+   * @param doc  Lucene-Document, von dem ueber ein zu diesem Merkmal
    *    gehoerigen Field der Wert ausgelesen wird
    */
-  public void leseMerkmalAusLuceneDocument(Document doc) {   
-    this.wert = doc.get(FELDNAME);  
+  public void leseMerkmalAusLuceneDocument(Document doc) {
+    this.wert = doc.get(FELDNAME);
+    try{
+    this.thumbnail = 
+	ImageIO.read(new ByteArrayInputStream((byte[]) this.wert));
+    } catch(IOException e) {
+      //FEHLERBEHANDLUNG
+    }
   }
-    
+  
   /**
    * Erzeuge aus diesem Merkmal ein entsprechendes Lucene-Field.
    * @return ein entsprechendes Lucene-Field
    */
   public Field erzeugeLuceneField() {
-    return new Field(FELDNAME, this.getWert().toString(), Field.Store.YES, 
-      Field.Index.UN_TOKENIZED);
+    return new Field(FELDNAME, (byte[]) this.getWert(), Field.Store.YES);
   }
   
   /**
@@ -92,14 +113,14 @@ public class ThumbnailMerkmal extends Merkmal {
    * @return <code>true</code>, wenn dieses Merkmal editierbar sein soll
    */
   public boolean istEditierbar() {
-    return false; 
-  }  
+    return false;
+  }
   
-  /** 
+  /**
    * Liefert das Thumbnail des Bilddokumentes.
    * @return  Thumbnail des Bilddokumentes
    */
-  public Image getThumbnail() {
+  public BufferedImage getThumbnail() {
     return this.thumbnail;
   }
 }
