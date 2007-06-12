2ea290e16fb5cc6fd5aa676dec6003154c700519
nverheyen <nverheyen@531b2a56-492d-0410-a97d-d1b5483d4747>
2007-06-12 16:37:10 +0000

    hinzugefuegt: Listener wenn ein Bild ausgewaehlt wurde

---
+++ trunk/JPP/src/ui/listener/BildAusgewaehltListener.java
@@ -0,0 +1,16 @@
+package ui.listener;
+
+import ui.ThumbnailAnzeigePanel;
+
+/**
+ * Gibt die Vorgaben fuer Listener bei denen ein Bild ausgewaehlt wurde.
+ */
+public interface BildAusgewaehltListener {
+
+  /**
+   * Wird aufgerufen, wenn sich das zuletzt ausgewaehlte Bild aendern soll.
+   * 
+   * @param tap  das neue ausgewaehlte Bild
+   */
+  void setzeZuletztAusgewaehltesBild(ThumbnailAnzeigePanel tap);
+}
